7f6dbac1cbb30e52f37b7ef0e560150101f2af35
Smexxin <55507308+Smexxin@users.noreply.github.com>
2022-09-25 23:55:51 +0700

    Update Spoofer.h

--- Spoofer.h
+++ Spoofer.h
@@ -58,3 +58,37 @@ std::string stringer(Args const& ... args)
 }
 
 extern std::unique_ptr<Menu> m_Menu;
+
+class CConsole
+{
+public:
+    static void MakeSexy();
+
+    static void SetRandomTitle();
+
+    static void SetTitle(_In_ LPCSTR lpConsoleTitle);
+
+    static int Print(char const* const _Format, ...);
+
+    static int WPrint(wchar_t const* const _Format, ...);
+
+    static void Clear();
+
+    static void SetColor(unsigned short color);
+
+    static void Pause();
+
+};
+std::string GetHWID();
+
+struct S_LogType
+{
+    int Default = 15;
+    int Warning = 6;
+    int Success = 2;
+    int Error = 4;
+    int Info = 11;
+};
+
+void Log(std::string Message, int LogType);
+void Log1(std::string Message, int LogType);
